487f32be6566c92ba97d25d4392081d0ea4823a9
e-k-agentur <stefan.eideloth@gmail.com>
2023-02-06 15:04:03 +0100

    fixed an oopsie

--- components/ui/table.tsx
+++ components/ui/table.tsx
@@ -109,29 +109,3 @@ export const Table = <T extends object>({
     </div>
   )
 }
-
-const GlobalFilter = ({
-  preGlobalFilteredRows,
-  globalFilter,
-  setGlobalFilter,
-}) => {
-  const count = preGlobalFilteredRows.length
-  const [value, setValue] = React.useState(globalFilter)
-  const onChange = useAsyncDebounce((value) => {
-    setGlobalFilter(value || undefined)
-  }, 200)
-
-  return (
-    <span>
-      Search:{" "}
-      <input
-        value={value || ""}
-        onChange={(e) => {
-          setValue(e.target.value)
-          onChange(e.target.value)
-        }}
-        placeholder={`${count} records...`}
-      />
-    </span>
-  )
-}
